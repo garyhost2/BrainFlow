4c179a6f50f340bcd723dc4321bea52f3fd90399
azizkhadraoui <142397889+azizkhadraoui@users.noreply.github.com>
2026-04-22 21:47:16 +0100

    up

--- eval_baseline.sh
+++ eval_baseline.sh
@@ -5,9 +5,9 @@ cd ~/BrainFlow
 
 module load cuda12.1/toolkit/12.1.1
 
-# Activate environment
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate brainflow
+# Activate environment (already in brainflow env)
+# source ~/miniconda3/etc/profile.d/conda.sh
+# conda activate brainflow
 
 echo "=== Evaluating Baseline Model ==="
 echo "Checkpoint: outputs/best_combined_v5.pt"
